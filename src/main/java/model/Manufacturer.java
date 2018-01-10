9c6937d006f25d4bda8da6dabbd07a67224b2fe1
Shumskih <music.dll@mail.ru>
2018-01-10 09:44:03 +0300

    __

--- src/main/java/model/Manufacturer.java
+++ src/main/java/model/Manufacturer.java
@@ -1,7 +1,5 @@
 package model;
 
-import org.hibernate.annotations.Type;
-
 import javax.persistence.*;
 import java.util.Set;
 import java.util.UUID;
@@ -13,7 +11,6 @@ public class Manufacturer {
     @Id
     @Column(name = "id")
     @GeneratedValue
-    @Type(type = "uuid-char")
     private UUID id;
 
     @Column(name = "name")
